c8c57cc1e06f3383cce4f24e49f86e4deaf53936
Boss-matsunaga <32508729+Boss-matsunaga@users.noreply.github.com>
2018-05-03 11:58:52 +0900

    Add files via upload

---
+++ push.js
@@ -0,0 +1,12 @@
+Push.create('Hello World!')
+
+Push.create("Hello world!", {
+    body: "How's it hangin'?",
+    icon: '/icon.png',
+    timeout: 4000,
+    onClick: function () {
+        window.focus();
+        this.close();
+    }
+});
+
